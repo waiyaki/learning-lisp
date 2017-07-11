8006cd676edabf03cdc612bcdc130efd5d224a35
James Muturi <james.muturi@andela.com>
2017-07-11 12:56:10 +0300

    Magic and Macros

---
+++ land-of-lisp/svg.lisp
@@ -0,0 +1,104 @@
+;;;; A DSL for writing SVG files.
+
+;; Helper function for printing a single opening or closing XML tag.
+(defun print-tag (name alst closingp)
+  (princ #\<)
+  (when closingp
+    (princ #\/))
+  (princ (string-downcase name))
+  (mapc (lambda (attributes)
+          (format t " ~a=\"~a\"" (string-downcase (car attributes)) (cdr attributes)))
+        alst)
+  (princ #\>))
+
+;; Tag macro for generating tags
+(defmacro tag (name attrs &body body)
+  `(progn (print-tag ',name
+                     (list ,@(mapcar (lambda (x)
+                                       `(cons ',(car x) ,(cdr x)))
+                                     (pairs attrs)))
+                     nil)
+          ,@body
+          (print-tag ',name nil t)))
+
+
+;; Group items into a list of pairs
+(defun pairs (lst)
+  (labels ((f (lst acc)
+             (split lst
+                    (if tail
+                        (f (cdr tail) (cons (cons head (car tail)) acc))
+                        (reverse acc))
+                    (reverse acc))))
+    (f lst nil)))
+
+;; Macro for splitting a list
+(defmacro split (val yes no)
+  (let ((g (gensym)))
+    `(let ((,g ,val))
+       (if ,g
+           (let ((head (car ,g))
+                 (tail (cdr ,g)))
+             ,yes)
+           ,no))))
+
+;; svg macro that embodies an entire SVG image
+(defmacro svg (&body body)
+  `(tag svg (xmlns "http://www.w3.org/2000/svg"
+                   "xmlns:xlink" "http://www.w3.org/1999/xlink")
+     ,@body))
+
+;; Generate light/dark variants of a color.
+;; 'color' is an RGB list. (255 0 0) is bright red.
+(defun brightness (color amount)
+  (mapcar (lambda (x)
+            (min 255 (max 0 (+ x amount))))
+          color))
+
+;; Set the style of an SVG picture element
+(defun svg-style (color)
+  (format nil
+          "~{fill:rgb(~a,~a,~a);stroke:rgb(~a,~a,~a)~}"
+          (append color
+                  (brightness color -100))))
+
+;; Draw a circle.
+(defun circle (center radius color)
+  (tag circle (cx (car center)
+                  cy (cdr center)
+                  r radius
+                  style (svg-style color))))
+
+;; Draw an arbitrary polygon
+(defun polygon (points color)
+  (tag polygon (points (format nil
+                               "~{~a,~a ~}"
+                               (mapcan (lambda (tp)
+                                         (list (car tp) (cdr tp)))
+                                       points))
+                       style (svg-style color))))
+
+;; Generate a random walk.
+;; A random walk is a graph you'd get if you were to flip a coin and then
+;; move either up or down a step.
+(defun random-walk (value length)
+  (unless (zerop length)
+    (cons value
+          (random-walk (if (zerop (random 2))
+                           (1- value)
+                           (1+ value))
+                       (1- length)))))
+
+;; Draw random walks in a picture
+(defun draw-random-walks ()
+  (with-open-file (*standard-output* "random_walk.svg"
+                                     :direction :output
+                                     :if-exists :supersede)
+    (svg (loop repeat 10
+            do (polygon (append '((0 . 200))
+                                (loop for x from 0
+                                   for y in (random-walk 100 400)
+                                   collect (cons x y))
+                                '((400 . 200)))
+                        (loop repeat 3
+                           collect (random 256)))))))
--- land-of-lisp/wizards-apprentice.lisp
+++ land-of-lisp/wizards-apprentice.lisp
@@ -109,3 +109,73 @@
                              nil)
                  'string))
   (fresh-line))
+
+;; Allow players to weld chain to bucket if they have it
+(defun have (object)
+  (member object (inventory)))
+
+(defparameter *chain-welded* nil)
+
+(defun weld-prev (subject object)
+  (if (and (eq *location* 'attic)
+           (eq subject 'chain)
+           (eq object 'bucket)
+           (have 'chain)
+           (have 'bucket)
+           (not *chain-welded*))
+      (progn (setf *chain-welded* t)
+             '(the chain is now securely welded to the bucket.))
+      '(you cannot weld like that.)))
+
+(pushnew 'weld-prev *allowed-commands*)
+
+;; Let the player dunk the bucket into the well to fill it with water.
+(defparameter *bucket-filled* nil)
+
+(defun dunk-prev (subject object)
+  (if (and (eq *location* 'garden)
+           (eq subject 'bucket)
+           (eq object 'well)
+           (have 'bucket)
+           *chain-welded*)
+      (progn (setf *bucket-filled* t)
+             '(the bucket is now full of water.))
+      '(you cannot dunk like that.)))
+
+(pushnew 'dunk-prev *allowed-commands*)
+
+;; Macro to generate game actions (e.g dunk-prev, weld)
+(defmacro game-action (command subj obj place &body body)
+  `(progn (defun ,command (subject object)
+            (if (and (eq *location* ',place)
+                     (eq subject ',subj)
+                     (eq object ',obj)
+                     (have ',subj))
+                ,@body
+                '(I cannot ,command like that.)))
+          (pushnew ',command *allowed-commands*)))
+
+;; Use macro to define game actions
+(setf *chain-welded* nil)
+
+(game-action weld chain bucket attic
+  (if (and (have 'bucket) (not *chain-welded*))
+      (progn (setf *chain-welded* t)
+             '(the chain is now securely welded to the bucket.))
+      '(you do not have a bucket.)))
+
+(setf *bucket-filled* nil)
+
+(game-action dunk bucket well garden
+  (if *chain-welded*
+      (progn (setf *bucket-filled* t)
+             '(the bucket is now full of water.))
+      '(the water level is too low to reach.)))
+
+(game-action splash bucket wizard living-room
+  (cond ((not *bucket-filled*) '(the bucket has nothing in it.))
+        ((have 'frog) '(the wizard awakens and sees that you stole his frog.
+                        he is so upset he banishes you to the
+                        netherworlds- you lose! the end.))
+        (t '(the wizard awakens from his slumber and greets you warmly.
+             he hands you the magic low-carb donut- you win! the end.))))
