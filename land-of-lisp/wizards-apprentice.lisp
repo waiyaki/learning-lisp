693439e3edefc05e67612d55dadfb1cff51e35ce
James Muturi <james.muturi@andela.com>
2017-07-12 05:39:24 +0300

    Make game-action macro use gensyms

--- land-of-lisp/wizards-apprentice.lisp
+++ land-of-lisp/wizards-apprentice.lisp
@@ -146,14 +146,16 @@
 
 ;; Macro to generate game actions (e.g dunk-prev, weld)
 (defmacro game-action (command subj obj place &body body)
-  `(progn (defun ,command (subject object)
-            (if (and (eq *location* ',place)
-                     (eq subject ',subj)
-                     (eq object ',obj)
-                     (have ',subj))
-                ,@body
-                '(I cannot ,command like that.)))
-          (pushnew ',command *allowed-commands*)))
+  (let ((subject (gensym))
+        (object (gensym)))
+    `(progn (defun ,command (,subject ,object)
+              (if (and (eq *location* ',place)
+                       (eq ,subject ',subj)
+                       (eq ,object ',obj)
+                       (have ',subj))
+                  ,@body
+                  '(I cannot ,command like that.)))
+            (pushnew ',command *allowed-commands*))))
 
 ;; Use macro to define game actions
 (setf *chain-welded* nil)
