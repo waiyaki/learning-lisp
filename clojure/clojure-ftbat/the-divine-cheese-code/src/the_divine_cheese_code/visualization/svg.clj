cbb9a4a410def7f8bc316813a3b402f5dddd8d44
James Muturi <james.muturi@andela.com>
2017-09-24 12:13:27 +0300

    Code organization - namespaces, filesystem

---
+++ clojure/clojure-ftbat/the-divine-cheese-code/src/the_divine_cheese_code/visualization/svg.clj
@@ -0,0 +1,57 @@
+(ns the-divine-cheese-code.visualization.svg)
+
+(defn comparator-over-maps
+  [comparison-fn ks]
+  (fn [maps]
+    (zipmap ks
+            (map (fn [k] (apply comparison-fn (map k maps)))
+                 ks))))
+
+(def min (comparator-over-maps clojure.core/min [:lat :lng]))
+(def max (comparator-over-maps clojure.core/max [:lat :lng]))
+
+(defn translate-to-00
+  [locations]
+  (let [mincoords (min locations)]
+    (map #(merge-with - % mincoords) locations)))
+
+(defn scale
+  [width height locations]
+  (let [maxcoords (max locations)
+        ratio {:lat (/ height (:lat maxcoords))
+               :lng (/ width (:lng maxcoords))}]
+    (map #(merge-with * % ratio) locations)))
+
+(defn latlng->point
+  "Convert a lat/lng map to a comma-separated string."
+  [latlng]
+  (str (:lng latlng) "," (:lat latlng)))
+
+(defn points
+  "Given a seq of lat/lng maps, return a string of points joined by space."
+  [locations]
+  (clojure.string/join " " (map latlng->point locations)))
+
+(defn line
+  [points]
+  (str "<polyline points=\"" points "\" />"))
+
+(defn transform
+  "Chain other functions"
+  [width height locations]
+  (->> locations
+       translate-to-00
+       (scale width height)))
+
+(defn xml
+  "svg 'template' that also flips the coordinate system."
+  [width height locations]
+  (str "<svg height=\"" height "\" width=\"" width "\">"
+       ;; These two <g> tags flip the coordinate system
+       "<g transform=\"translate(0," height ")\">"
+       "<g transform=\"scale(1,-1)\">"
+       (-> (transform width height locations)
+           points
+           line)
+       "</g></g>"
+       "</svg>"))
